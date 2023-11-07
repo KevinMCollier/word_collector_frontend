540f030ea351a377a082e873bd541b923d60991a
kevin collier <collier.kevin.m@gmail.com>
2023-11-07 15:44:23 +0900

    tested that tailwind is correcting - and it is

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import MainLayout from '../../layouts/MainLayout';
 const HomePage = () => {
   return (
     <MainLayout>
-      <h1>Hey Kyo!</h1>
+      <h1 className="text-blue-500">Hey Kyo!</h1>
     </MainLayout>
   );
 };
