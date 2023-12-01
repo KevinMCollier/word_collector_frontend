f3babe5c303c9bd0544ead961fe71b471c4c895c
kevin collier <collier.kevin.m@gmail.com>
2023-12-01 17:35:32 +0900

    created basic html/css structure for sidebar

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,7 +1,24 @@
 const Sidebar = () => {
   return (
-    <div>
-      Test
+    <div className="bg-gray-200 h-screen w-1/6 min-w-[200px] fixed flex flex-col items-start p-4">
+      {/* Create a gray sidebar that covers the vertical height of the screen and approx. 1/6 width of the screen */}
+      {/* Include names of tags (backend -> context API) */}
+
+      <div className="flex justify-center w-full mb-6">
+        <div>User Avatar Placeholder</div>
+      </div>
+
+      <div className="mb-4">
+        <div>Recent Categories Placeholder</div>
+      </div>
+
+      <div className="mb-4">
+        <div>My Categories Placeholder</div>
+      </div>
+
+      <div className="mb-4">
+        <div>Recommended Categories Placeholder</div>
+      </div>
     </div>
   )
 }
