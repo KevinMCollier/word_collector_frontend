5609e88f2e28a06c5c0437ee111faa2acb861aae
kevin collier <collier.kevin.m@gmail.com>
2023-12-14 20:44:40 +0900

    updated extraction logic for getting email and auth token

--- src/services/authService.js
+++ src/services/authService.js
@@ -7,25 +7,27 @@ export const login = async (credentials) => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ user: credentials }),
     });
+
     console.log('Response Status:', response.status);
-    console.log('Response Data:', await response.clone().json()); // Use clone() to avoid consuming the response
+
+    const data = await response.json();
+    console.log('Extracted Data:', data); // Log the extracted data
 
     if (!response.ok) {
       throw new Error('Login failed');
     }
 
-    const data = await response.json();
-
     // Check if the response contains a user and an authentication token
-    if (!data || !data.token) {
+    if (!data.user || !data.user.authentication_token) {
       throw new Error('Invalid login credentials');
     }
 
     // Extract the authentication token and user data
-    const authToken = data.authentication_token;
-    const user = data;
+    const authToken = data.user.authentication_token;
+    const userEmail = data.user.email;
+    // Extract other user data as needed
 
-    return { user, token: authToken };
+    return { user: { email: userEmail }, token: authToken }; // Update as needed based on your requirements
   } catch (error) {
     console.error('Error during login:', error);
     throw error;
